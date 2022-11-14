eb49167a841b2a0a309c73ce56f627dcb3a9c233
alexe <prograbota@mail.ru>
2022-11-14 17:46:20 +0300

    исправл ошиб

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +14,11 @@ import java.util.List;
 @Controller
 public class CarsController {
 
+
+    CarService carService = new CarServiceImpl();
     @GetMapping(value = "cars")
     public String printCars(@RequestParam(defaultValue = "5") Integer count, ModelMap model) {
-        CarService carServiceImpl = new CarServiceImpl();
-        List<Car> qCars = carServiceImpl.carList(count);
-        model.addAttribute("cars", qCars);
+        model.addAttribute("cars", carService.carList(count));
         return "cars";
     }
 }
