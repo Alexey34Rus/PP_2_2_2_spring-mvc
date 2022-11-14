1dc421df4b292146cf0ecf0c0ffea0ef8c585765
alexe <prograbota@mail.ru>
2022-11-14 17:47:36 +0300

    исправл ошиб

---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,25 @@
+package web.service;
+
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarServiceImpl implements CarService{
+
+    @Override
+    public List<Car> carList(Integer count) {
+        List<Car> carList;
+        {
+
+            carList = new ArrayList<>();
+            carList.add(new Car("Lada", "kalina", 2012));
+            carList.add(new Car("Honda", "civic", 2007));
+            carList.add(new Car("Jeep", "charok", 1998));
+            carList.add(new Car("BMW", "X5", 2010));
+            carList.add(new Car("Opel", "Kadet", 2001));
+        }
+        return carList.stream().limit(count).collect(Collectors.toList());
+    }
+}
